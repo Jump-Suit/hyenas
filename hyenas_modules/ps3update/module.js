d246a58f44fb49c21778af0b7bba583925a94f4b
JumpSuit <JumpSuitGamer@gmail.com>
2021-03-24 00:44:26 -0500

    Additional ps3-updatelist.txts for other regions.
    Adds support for 2 other regions of ps3 updates: Russian and Australian/New Zealand regions.

--- hyenas_modules/ps3update/module.js
+++ hyenas_modules/ps3update/module.js
@@ -1,11 +1,18 @@
 module.exports = (hyenas) => {
     hyenas.get('/update/ps3/list/us/ps3-updatelist.txt', (req, res) => 
-        res.sendFile('./data/ps3-updatelist.txt', { root: __dirname }));
-    /*
+        res.sendFile('./data/list/us/ps3-updatelist.txt', { root: __dirname }));
+		
+	hyenas.get('/update/ps3/list/ru/ps3-updatelist.txt', (req, res) => 
+        res.sendFile('./data/list/ru/ps3-updatelist.txt', { root: __dirname }));
+		
+	hyenas.get('/update/ps3/list/au/ps3-updatelist.txt', (req, res) => 
+        res.sendFile('./data/list/au/ps3-updatelist.txt', { root: __dirname }));
+    
 	hyenas.get('/update/ps3/image/us/2020_1203_03373a581934f0d2b796156d2fb28b39/PS3UPDAT.PUP', (req, res) => 
         res.sendFile('./data/487/PS3UPDAT.PUP', { root: __dirname }));
-	*/
+		
+	/* Testing Hybrid Firmware 4.87.1
 	hyenas.get('/update/ps3/image/us/2020_1203_03373a581934f0d2b796156d2fb28b39/PS3UPDAT.PUP', (req, res) => 
         res.sendFile('./data/HFW_4871/HFW_4.87.1_PS3UPDAT.PUP', { root: __dirname }));
-	
+	*/
 };
