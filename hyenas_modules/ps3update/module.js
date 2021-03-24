04e7e3e104c386dfc11dd11c9b605d09ed1dbf19
JumpSuit <JumpSuitGamer@gmail.com>
2021-03-24 12:19:19 -0500

    Added 3 additional Update Regions.
    Regions added:
    Mexico, Brasil, and Korea.

--- hyenas_modules/ps3update/module.js
+++ hyenas_modules/ps3update/module.js
@@ -8,6 +8,15 @@ module.exports = (hyenas) => {
 	hyenas.get('/update/ps3/list/au/ps3-updatelist.txt', (req, res) => 
         res.sendFile('./data/list/au/ps3-updatelist.txt', { root: __dirname }));
     
+	hyenas.get('/update/ps3/list/br/ps3-updatelist.txt', (req, res) => 
+        res.sendFile('./data/list/br/ps3-updatelist.txt', { root: __dirname }));
+	
+	hyenas.get('/update/ps3/list/mx/ps3-updatelist.txt', (req, res) => 
+        res.sendFile('./data/list/mx/ps3-updatelist.txt', { root: __dirname }));
+	
+	hyenas.get('/update/ps3/list/kr/ps3-updatelist.txt', (req, res) => 
+        res.sendFile('./data/list/kr/ps3-updatelist.txt', { root: __dirname }));
+	
 	hyenas.get('/update/ps3/image/us/2020_1203_03373a581934f0d2b796156d2fb28b39/PS3UPDAT.PUP', (req, res) => 
         res.sendFile('./data/487/PS3UPDAT.PUP', { root: __dirname }));
 		
