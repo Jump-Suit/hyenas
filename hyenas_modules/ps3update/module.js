190aa53c9b97c6c71253e57f92b36eb090972806
JumpSuit <JumpSuitGamer@gmail.com>
2021-03-24 00:30:18 -0500

    Avatar prep, PS3UPDAT System Update prep.
    Preparations for System Update Mock requests. Added Hybrid Firmware 4.87.1 for testing against. Avatars added to static pool thanks to VTS_Tech. Avatars are unmapped in module.js for now.  Ignored .PUPs due to size.

--- hyenas_modules/ps3update/module.js
+++ hyenas_modules/ps3update/module.js
@@ -1,4 +1,11 @@
 module.exports = (hyenas) => {
     hyenas.get('/update/ps3/list/us/ps3-updatelist.txt', (req, res) => 
         res.sendFile('./data/ps3-updatelist.txt', { root: __dirname }));
+    /*
+	hyenas.get('/update/ps3/image/us/2020_1203_03373a581934f0d2b796156d2fb28b39/PS3UPDAT.PUP', (req, res) => 
+        res.sendFile('./data/487/PS3UPDAT.PUP', { root: __dirname }));
+	*/
+	hyenas.get('/update/ps3/image/us/2020_1203_03373a581934f0d2b796156d2fb28b39/PS3UPDAT.PUP', (req, res) => 
+        res.sendFile('./data/HFW_4871/HFW_4.87.1_PS3UPDAT.PUP', { root: __dirname }));
+	
 };
